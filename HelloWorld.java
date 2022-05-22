5b7ef2e51824e2e06940217a5062876790185bff
Nihar-Sawant <niharsawa11223@gmail.com>
2022-05-22 17:33:38 +0530

    Day 1 - Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 public class HelloWorld{
     public static void main(String[] str) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!!!");
     }
 }
